e1c86a299920cc6d1a6a4b7a56e0305768462a3f
Jacob Hands <jacob@gogit.io>
2024-01-12 15:25:03 -0600

    Use node-fetch

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-// import fetch from 'node-fetch'
-import { fetch, setGlobalDispatcher, Agent } from 'undici'
+import fetch from 'node-fetch'
+// import { fetch, setGlobalDispatcher, Agent } from 'undici'
 import PQueue from 'p-queue'
-setGlobalDispatcher(new Agent({ connect: { timeout: 30_000, keepAlive: true } }))
+// setGlobalDispatcher(new Agent({ connect: { timeout: 30_000, keepAlive: true } }))
 
 async function main() {
 	let httpErrors = 0
